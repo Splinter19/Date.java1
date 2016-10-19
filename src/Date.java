509a2635ecbc55853598466634e4b0f6fbb1b9c1
Tehnikumi Õpilane <opilane@tptlive.ee>
2016-10-19 15:24:44 +0300

    Initial commit

---
+++ src/Date.java
@@ -0,0 +1,31 @@
+/**
+ * Created by opilane on 19.10.2016.
+ */
+public class Date {
+    public static void main(String[] args){
+        String firstLine="Wednesday";
+        System.out.print(firstLine);
+        System.out.print(", ");
+        int date=19;
+        int year=2016;
+        String secondLine="October";
+        System.out.print(secondLine);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(", ");
+        System.out.println(year);
+        System.out.print(firstLine);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(" ");
+        System.out.print(secondLine);
+        System.out.print(" ");
+        System.out.print(year);
+
+
+
+
+
+
+    }
+}
